cbdd852a7b6b26e51ea184c531717e60f70dffed
Felox <feloxlopez05@gmail.com>
2021-12-11 17:57:03 +0100

    auto theme select confirm.html

--- assets/scripts/cnf.js
+++ assets/scripts/cnf.js
@@ -11,4 +11,3 @@ var storedTheme = localStorage.getItem('theme') || (window.matchMedia("(prefers-
 if (storedTheme) {
     document.documentElement.setAttribute('data-theme', storedTheme);
 }
-document.documentElement.setAttribute('data-theme', "light");
