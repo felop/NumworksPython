96b73e68bf09920d330bc4f493cbd9d50f81bae9
Felox <feloxlopez05@gmail.com>
2021-12-11 15:45:00 +0100

    updated confirm page

--- confirm.php
+++ confirm.php
@@ -5,13 +5,24 @@
     <title>Don't Work - Confirm</title>
     <link rel="stylesheet" href="assets/css/confirm.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+    <script src="assets/scripts/cnf.js"></script>
   </head>
   <body>
     <div id="menu">
       <h2 id="menu_name">Don't Work</h2>
-      <img id="numworksLogo" src="pics/NumworksUsb.png" alt="">
+      <img id="numworksLogo" src="assets/pics/NumworksUsb.png" alt="">
     </div>
     <div id="container">
+
+      <div id="txtConfirmation">
+        <form id="txtForm" action="confirm.html" method="POST">
+          <textarea id="txtConfirmationArea" name="text" cols="42" rows="10"><?php echo htmlspecialchars($_POST["text"]); ?></textarea>
+        </form>
+      </div>
+
+      <div id="paypalBox">
+
+      </div>
 
     </div>
   </body>
